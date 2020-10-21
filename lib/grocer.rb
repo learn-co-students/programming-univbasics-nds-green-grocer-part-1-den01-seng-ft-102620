8fdb79e6bd79ddda657693ba451f531cc070a775
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-21 15:42:05 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -16,22 +16,22 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  count_cart = []
-  cart.each do |grocery_item| #iterates through each item in the cart 
-    current_item = find_item_by_name_in_collection(grocery_item[:item], count_cart)
-    if current_item
-      count_cart_index = 0
-      count_cart.each do |count_cart_item|
-        if count_cart_item[:item] == current_item[:item]
-          count_cart_item[:count] += 1
-        end
-        count_cart_index += 1
-      end
-    else 
-      grocery_item[:count] = 1 
-      count_cart.push(grocery_item)
-    end
-  end 
+  count_cart = [] #make an empty result array
+  cart.each do |grocery_item| #iterate through each item in the cart 
+    current_item = find_item_by_name_in_collection(grocery_item[:item], count_cart) #create a variable that stores the current grocery item in the count_cart array using the previous function
+    if current_item #take a look at the current item object, as defined by the current_item variable
+      count_cart.each do |count_cart_item| #for each item in the count cart 
+        count_cart_index = 0 #start at 0
+        if count_cart_item[:item] == current_item[:item] #if the count_cart item name matches the current item name
+          count_cart_item[:count] += 1 #increase the count by 1
+        end #end if loop
+        count_cart_index += 1 #iterate through the count cart and go to the next item
+      end #end the count_cart.each loop
+    else #otherwise
+      grocery_item[:count] = 1 #add a count key to the grocery_item with a value of 1
+      count_cart.push(grocery_item) #push that item into the count_cart
+    end #end else loop
+  end #end iteration through the cart
   # takes in an array of objects
   # create a result object 
   # iterate over the array of objects
@@ -42,8 +42,8 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-  return count_cart
-end
+  return count_cart #return the new array
+end #end the method
 
 =begin
 NOTES:
