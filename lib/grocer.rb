b6df18a9275451e9adc686b96b5a5ee7c479e92c
Jordan Panasewicz <jbpanasewicz@gmail.com>
2020-10-22 00:18:02 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -9,6 +9,7 @@ def find_item_by_name_in_collection(name, collection)
   return nil
 end
 
+=begin
 def consolidate_cart(cart)
   answer = []
   i = 0 
@@ -29,6 +30,29 @@ def consolidate_cart(cart)
   end
   answer
 end
+=end
 
+def consolidate_cart(cart)
+  index = 0
+  new_cart = []
+  
+  cart.each do |grocery_item|
+    current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
+    if current_item
+      new_cart_index = 0
+      new_cart.each do |new_cart_item|
+        if new_cart_item[:item] === current_item[:item]
+          new_cart_item[:count] += 1
+        end
+        new_cart_index += 1
+      end
+    else
+      grocery_item[:count] = 1
+      new_cart << grocery_item
+    end
+    index += 1
+  end
+  new_cart
+end
 
 
